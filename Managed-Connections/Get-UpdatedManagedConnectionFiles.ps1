5ba6d320b40d2430ef0cc2e358a60a695336a451
Samuel Kastberg <samuel.kastberg@live.se>
2022-06-13 21:34:11 +0100

    Corrected scriptname for raw

--- Managed-Connections/Get-UpdatedManagedConnectionFiles.ps1
+++ Managed-Connections/Get-UpdatedManagedConnectionFiles.ps1
@@ -25,7 +25,7 @@ Connect-AzAccount -Tenant $settings.values.WORKFLOWS_TENANT_ID | Out-Null
 $ctx = Set-AzContext -Subscription $settings.values.WORKFLOWS_SUBSCRIPTION_ID
 
 . "$PSScriptRoot\Generate-Connections.ps1" -resourceGroup $settings.values.WORKFLOWS_RESOURCE_GROUP_NAME -outputLocation "$destinationFolder\$prefix.connections.az.json"
-. "$PSScriptRoot\Generate-ConnectionsFileRaw.ps1" -resourceGroup $settings.values.WORKFLOWS_RESOURCE_GROUP_NAME -outputLocation "$destinationFolder\$prefix.connections.code.json"
+. "$PSScriptRoot\Generate-ConnectionsRaw.ps1" -resourceGroup $settings.values.WORKFLOWS_RESOURCE_GROUP_NAME -outputLocation "$destinationFolder\$prefix.connections.code.json"
 
 $connections = Get-Content -Path "$destinationFolder\$prefix.connections.code.json" -Encoding utf8 | ConvertFrom-Json -Depth 10
 $tokens = ""
